fe4a6f59e2a306f48c64abef986933f02f611f3f
Toby Smithe <git@tsmithe.net>
2015-12-08 17:37:46 +0100

    Add square root UKF; also broken...

--- nds.py
+++ nds.py
@@ -75,8 +75,8 @@ def train_filter(filt, U, Y):
     n = len(U)
     X = []
     for i in range(n):
-        x_hat_bar, P_bar = filt.time_update(U[i])
-        x_hat, P = filt.measurement_update(Y[i], P_bar, x_hat_bar)
+        x_hat_bar = filt.time_update(U[i])
+        x_hat, P = filt.measurement_update(Y[i], x_hat_bar)
         print(i, x_hat)
         X.append(x_hat)
     return np.array(X)
@@ -138,9 +138,11 @@ class EKF:
         self.A = self.dF_dx(self.x_hat, u)
         A = self.A
         P_bar = A.dot(self.P).dot(A.T) + self.R_v
-        return x_hat_bar, P_bar
+        self.P_bar = P_bar
+        return x_hat_bar
 
-    def measurement_update(self, y, P_bar, x_hat_bar):
+    def measurement_update(self, y, x_hat_bar):
+        P_bar = P_bar
         A = self.A
         C = self.dH_dx(x_hat_bar)
         K = P_bar.dot(C.T).dot(np.linalg.inv(C.dot(P_bar).dot(C.T) + self.R_n))
@@ -229,24 +231,24 @@ class UKF:
         y_hat_bar = np.zeros(self.M)
         for i in range(2*self.L + 1):
             Y[i] = self.H(sigma_points[i])
-        for i in range(2*self.L):
+        for i in range(2*self.L + 1):
             y_hat_bar += self.weights_m[i] * Y[i]
 
-        self.measurement_params = (sigma_points, Y, y_hat_bar)
+        self.measurement_params = (sigma_points, P_bar, Y, y_hat_bar)
 
-        return x_hat_bar, P_bar
+        return x_hat_bar
 
-    def measurement_update(self, y, P_bar, x_hat_bar):
-        sigma_points, Y, y_hat_bar = self.measurement_params
+    def measurement_update(self, y, x_hat_bar):
+        sigma_points, P_bar, Y, y_hat_bar = self.measurement_params
 
         P_yy = np.array(self.R_n)
-        for i in range(2*self.L):
+        for i in range(2*self.L + 1):
             P_yy += self.weights_c[i] * np.outer(
                 Y[i] - y_hat_bar,
                 Y[i] - y_hat_bar)
 
         P_xy = np.zeros((self.L, self.M))
-        for i in range(2*self.L):
+        for i in range(2*self.L + 1):
             P_xy += self.weights_c[i] * np.outer(
                 sigma_points[i] - x_hat_bar,
                 Y[i] - y_hat_bar)
@@ -259,5 +261,147 @@ class UKF:
         self.x_hat = x_hat
         self.P = P
 
+        return x_hat, P
+
+
+class UKF_sqrt:
+    """
+    Assume
+      x_{k+1} = F(x_k, u_k) + v
+      y_k = H(x_k) + n
+    where v, n are Gaussian noise processes with mean 0
+     and covariances R_v, R_n.    
+    """
+    def weights_m(self):
+        w = np.zeros(2*self.L + 1)
+        w[0] = self.Lambda / (self.L + self.Lambda)
+        for i in range(1, 2*self.L + 1):
+            w[i] = 1 / (2*(self.L + self.Lambda))
+        return w
+
+    def weights_c(self):
+        w = self.weights_m #()
+        w[0] += 1 - self.alpha**2 + self.beta
+        return w
+
+    def __init__(self, F, H, R_v, R_n, x_hat_0, P_0, alpha=1):
+        """
+        x_hat_0 and P_0 are initial estimates of the state mean and covariance.
+        alpha determines the spread of sigma points, and should be small...
+        """
+        self.F = F
+        self.H = H
+        self.R_v = R_v
+        self.R_n = R_n
+        self.x_hat = x_hat_0
+        self.P = P_0
+        self.S = np.linalg.cholesky(P_0)
+        self.sqrt_R_v = np.linalg.cholesky(R_v)
+        self.sqrt_R_n = np.linalg.cholesky(R_n)
+        self.L = len(x_hat_0)
+        self.M = len(H(x_hat_0))
+        self.alpha = alpha
+        self.beta = 2
+        kappa = 3 - self.L
+        self.kappa = kappa
+        self.Lambda = alpha**2 * (self.L + kappa) - self.L
+
+        # NB: these don't change while we don't augment the sigma points
+        self.weights_m = self.weights_m()
+        self.weights_c = self.weights_c()
+
+    def sigma_points(self, x, S):
+        # nb: better to *augment* the sigma points at each k, not recompute
+        #     but this should work fine
+        sqrt = np.sqrt(self.L + self.Lambda) * S
+        sigma_points = np.zeros((2*self.L + 1, self.L))
+        sigma_points[0, :] = x
+        for i in range(1, self.L+1):
+            sigma_points[i, :] = x + sqrt[:, i-1]
+        for i in range(self.L):
+            sigma_points[self.L+i+1, :] = x - sqrt[:, i]
+        return sigma_points
+
+    def time_update(self, u):
+        sigma_points = self.sigma_points(self.x_hat, self.S)
+
+        sigma_star = np.zeros((2*self.L + 1, self.L))
+        for i in range(2*self.L + 1):
+            sigma_star[i] = self.F(sigma_points[i], u)
+
+        x_hat_bar = np.zeros(self.L)
+        for i in range(2*self.L + 1):
+            x_hat_bar += self.weights_m[i] * sigma_star[i]
+
+        S_bar = np.array(sigma_star)
+        for i in range(1, 2*self.L + 1):
+            S_bar[i] -= x_hat_bar
+        S_bar *= np.sqrt(self.weights_c[1])
+        print(S_bar.shape, self.sqrt_R_v.shape)
+        S_bar = np.c_[S_bar.T, self.sqrt_R_v]
+        q, r = np.linalg.qr(S_bar)
+        S_bar = r + np.sqrt(self.weights_c[0]) * np.outer(
+            sigma_star[0] - x_hat_bar,
+            sigma_star[0] - x_hat_bar)
+
+        # P_bar = np.array(self.R_v)
+        # for i in range(2*self.L + 1):
+        #     P_bar += self.weights_c[i] * np.outer(
+        #         sigma_star[i] - x_hat_bar,
+        #         sigma_star[i] - x_hat_bar)
+
+        sigma_points = self.sigma_points(x_hat_bar, S_bar)
+        
+        Y = np.zeros((2*self.L + 1, self.M))
+        y_hat_bar = np.zeros(self.M)
+        for i in range(2*self.L + 1):
+            Y[i] = self.H(sigma_points[i])
+        for i in range(2*self.L):
+            y_hat_bar += self.weights_m[i] * Y[i]
+
+        self.measurement_params = (sigma_points, S_bar, Y, y_hat_bar)
+
+        return x_hat_bar
+
+    def measurement_update(self, y, S_bar, x_hat_bar):
+        sigma_points, S_bar, Y, y_hat_bar = self.measurement_params
+
+        S_y = np.array(Y)
+        for i in range(1, 2*self.L + 1):
+            S_y[i] -= y_hat_bar
+        S_y *= np.sqrt(self.weights_c[1])
+        S_y = np.c_[S_y, self.sqrt_R_n]
+        q, r = np.linalg.qr(S_y)
+        S_y = r + np.sqrt(self.weights_c[0]) * np.outer(
+            Y[0] - y_hat_bar,
+            Y[0] - y_hat_bar)
+
+        # P_yy = np.array(self.R_n)
+        # for i in range(2*self.L):
+        #     P_yy += self.weights_c[i] * np.outer(
+        #         Y[i] - y_hat_bar,
+        #         Y[i] - y_hat_bar)
+
+        P_xy = np.zeros((self.L, self.M))
+        for i in range(2*self.L+1):
+            P_xy += self.weights_c[i] * np.outer(
+                sigma_points[i] - x_hat_bar,
+                Y[i] - y_hat_bar)
+
+        # b / a -> linalg.solve(a.T, b.T)
+        K = np.linalg.solve(S_y.T, np.linalg.solve(S_y, P_xy.T)).T
+
+        x_hat = x_hat_bar + K.dot(y - y_hat_bar)
+        self.x_hat = x_hat
+
+        U = K.dot(S_y)
+        S = np.array(S_bar)
+        for i in range(U.shape[1]):
+            S -= np.outer(U[:, i], U[:, i])
+        self.S = S
+
+        P = S.dot(S.T)
+        self.P = P
+        
         return x_hat, P
 
